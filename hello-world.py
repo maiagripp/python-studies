6f203d5e9ebe17a152ce3733cfaf4c2df25329e8
Claudia Maia <maiaandradec@gmail.com>
2022-06-07 19:03:22 -0300

    first commit

---
+++ hello-world.py
@@ -0,0 +1,20 @@
+print('minha primeira aplicação python')
+
+# string
+type('Hello World')
+
+# int
+type(1)
+
+# float
+type(1.0)
+
+# complex
+type( 2 + 3j)
+
+# variables
+message = 'hello, python'
+print(message)
+
+# variable pattern  
+variable_name = 'name'
