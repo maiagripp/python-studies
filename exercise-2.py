8468bdb692d056fcb1b56e73ebb94c2d96a54597
Claudia Maia <maiaandradec@gmail.com>
2022-08-21 12:49:31 -0300

    add new exercise

---
+++ exercise-2.py
@@ -0,0 +1,23 @@
+# exercise 2.3 
+name = "Claudia"
+print(name)
+
+# exercise 2.4
+a = 3
+b = 5
+
+print((2 * a) * (3 * b))
+
+# exercise 2.5
+a = 2
+b = 5
+c = 8
+
+print( a + b + c )
+
+#exercise 2.6
+income = 750
+r = 15
+
+print(income + (income * r/100 ))
+
