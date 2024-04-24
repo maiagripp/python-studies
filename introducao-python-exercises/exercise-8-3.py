ddca897a2e806c97db6b2a6e0c78c9927d86dc7f
Claudia Maia <maiaandradec@gmail.com>
2024-04-24 20:29:04 -0300

    added new exercises

---
+++ introducao-python-exercises/exercise-8-3.py
@@ -0,0 +1,7 @@
+def area_quadrado(number1):
+  return print(number1 * number1)
+
+
+
+area_quadrado(4)
+area_quadrado(9)
---
+++ introducao-python-exercises/mmc.py
@@ -0,0 +1,6 @@
+import mdc
+def mmc(a, b):
+    return abs(a*b)/mdc.mdc(a,b)
+    
+    
+print(mmc(4, 8))
