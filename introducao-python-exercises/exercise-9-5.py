db3697653ba19722bcdc422f6666b9b917f31d70
Claudia Maia <maiaandradec@gmail.com>
2024-09-09 19:56:06 -0300

    new exercises

--- introducao-python-exercises/exercise-9-5.py
+++ introducao-python-exercises/exercise-9-5.py
@@ -1,11 +1,10 @@
 with open("evens.txt", "r") as evens:
     evens_numbers = evens.readlines()
-    
+
     evens_numbers = [int(even_number.strip()) for even_number in evens_numbers]
-    
+
     reversed_evens = evens_numbers[::-1]
-    
+
 with open("reversed_evens.txt", "w") as file:
     for number in reversed_evens:
         file.write(f"{number}\n")
-       
